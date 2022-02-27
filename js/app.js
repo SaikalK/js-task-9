e282611ab5c3d66f7681e7516ec22b0fb6ce8000
Saikal Kuttubekova <saikal.kuttubekova.on@gmail.com>
2022-02-27 22:06:18 +0600

    need fix yet

--- js/app.js
+++ js/app.js
@@ -1,36 +1,58 @@
 let min = 1,
 max = 10,
-winningNum = 2,
-guessesLeft = 3;
+guessesLeft = 3,
+winningNum = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+
 
 const game = document.querySelector("#game"),
 minNum = document.querySelector(".min-num"),
 maxNum = document.querySelector(".max-num"),
 guessBtn = document.querySelector("#guess-btn"),
 guessInput = document.querySelector("#guess-input"),
-message = document.querySelector(".message");
+message = document.querySelector(".message"),
+messageBtn = document.querySelector(".messageBtn");
 
 minNum.textContent = min;
 maxNum.textContent = max;
 
-guessBtn.addEventListener('click', function() {
+guessBtn.addEventListener("click", function() {
     let guess = parseInt(guessInput.value);
 
     if (isNaN(guess) || guess < min || guess > max) {
-        setMessage('Нужно ввести число от ${min} до ${max}', "red");
+        setMessage("Нужно ввести число от ${min} до ${max}", "red");
+        guessInput.value = "";
+        guessBtn.addEventListener("click", () => window.location.reload());
     }
-
+   
     if (guess === winningNum) {
+        setMessage("Поздравляю! Вы угадали число: ${winningNum}!", "blue");
+        btnMessage("Играть снова", "blue");
         guessInput.disabled = true;
         guessInput.style.border = "2px solid blue";
-        setMessage('Поздравляю! Вы угадали число ${winningNum}', "blue");
-    } else {
-        
+        guessInput.value = "";
+        guessBtn.addEventListener("click", () => window.location.reload());
     }
+    else if (guess != winningNum) {
+        guessesLeft--;
+        setMessage('Вы не угадали число. Осталось попыток: ${guessesLeft}', "red");
+    }
+    if (guessesLeft === 0) {
+        setMessage('Вы не угадали загаданное число: ${winningNum}!', "red");
+        btnMessage('Играть снова', "red");
+        guessInput.disabled = true;
+        guessInput.style.border = "2px solid red";
+        guessInput.value = "";
+        guessBtn.addEventListener("click", () => window.location.reload());
+    }
+
+       function setMessage(msg, color) {
+           message.textContent = msg;
+           message.style.color = color;
+
+    }
+    function btnMessage(txt, color) {
+        guessBtn.innerText = txt;
+        guessInput.style.border = color;
+        guessBtn.style.color = color;
+      }
 });
-
-
-function setMessage(msg, color) {
-    message.textContent = msg,
-    message.style.color = color;
-}
